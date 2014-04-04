4c08fdc6d26d2c23ce92513115bfd69596d7bcca
reustonium <andrew.ruestow@gmail.com>
2014-04-03 22:25:02 -0400

    setting up heroku

---
+++ web.js
@@ -0,0 +1,11 @@
+var http = require('http')
+
+var server = http.createServer(function (req, res){
+	res.writeHead(200, {
+		'Content-Type':'text/plain'
+	})
+	res.write("hi!")
+	res.end()
+})
+
+server.listen(80)
