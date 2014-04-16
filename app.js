587e982ba529e447d6a82a0733cab000129ea955
reustonium <andrew.ruestow@gmail.com>
2014-04-15 21:34:54 -0400

    logging heroku env params

--- app.js
+++ app.js
@@ -35,4 +35,5 @@ app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+  console.log(process.env);
 });
