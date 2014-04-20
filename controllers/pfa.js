f0c31b8d90ee473d0d83993841654ff5a203af34
reustonium <andrew.ruestow@gmail.com>
2014-04-19 21:50:29 -0400

    more parsing work

--- controllers/pfa.js
+++ controllers/pfa.js
@@ -6,5 +6,33 @@ exports.trimData = function(rawData){
 	        data.push([time, rawData.rows[i][1]]);
 	    }
 	}
-	return data;
+
+	console.log(data);
+
+	var output = [];
+	for (var i=0; i<data.length; i++){
+	    if(i===0){output.push([data[i][0], data[i][1]]);}
+	    else{
+	        if(data[i][0]==data[i-1][0]){
+	            var val = data[i][1]+data[i-1][1];
+	            output.pop();
+	            output.push([data[i][0], val]);
+	        } else {
+	            output.push([data[i][0], data[i][1]]);
+	        }
+	    }
+	}
+	console.log(output);
+
+	var stuff = [];
+
+	for(var i=0; i<output.length; i++){
+	    if(i===0){stuff.push([output[i][0], output[i][1]]);}
+	    else{
+	        var val = output[i][1] + output[i-1][1];
+	        stuff.push([output[i][0], val]);
+	    }
+	}
+	console.log(stuff);
+	return output;
 }
