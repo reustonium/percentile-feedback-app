03921699417836d903a279b51340ac32aa6aa5ea
reustonium <andrew.ruestow@gmail.com>
2014-04-22 15:25:07 -0400

    migrated from d3 to highcharts

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,20 +1,52 @@
-var pfapp = angular.module('pfapp', ['angularCharts']);
+var pfapp = angular.module('pfapp', ['highcharts-ng']);
 
 function mainController($scope, $http) {
 	$scope.formData = {};
-	$scope.chartData = {
-		"series": ["daily"],
-		"data": [{
-			"x":"test",
-			"y":[0]
-		}]
-	};
+
+	$scope.chartConfig = {
+             //This is not a highcharts object. It just looks a little like one!
+             options: {
+                 //This is the Main Highcharts chart config. Any Highchart options are valid here.
+                 //will be ovverriden by values specified below.
+                 chart: {
+                     type: 'bar'
+                 },
+                 tooltip: {
+                     style: {
+                         padding: 10,
+                         fontWeight: 'bold'
+                     }
+                 },
+             },
+
+             //The below properties are watched separately for changes.
+
+             //Series object (optional) - a list of series using normal highcharts series options.
+             series: [{
+                 data: [10, 15, 12, 8, 7]
+             }],
+             //Title configuration (optional)
+             title: {
+                 text: 'Hello'
+             },
+             //Boolean to control showng loading status on chart (optional)
+             loading: false,
+             //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
+             //properties currentMin and currentMax provied 2-way binding to the chart's maximimum and minimum
+             xAxis: {
+              currentMin: 0,
+              currentMax: 20,
+              title: {text: 'values'}
+             },
+             //Whether to use HighStocks instead of HighCharts (optional). Defaults to false.
+             useHighStocks: false
+             }
 
 	// when submitting the KEY retrieve data
 	$scope.getData = function(){
 		$http.get('/api/daily/' + $scope.formData.text)
 		.success(function(data) {
-			$scope.chartData = data;
+			//$scope.chartData = data;
 			console.log(data);
 		})
 		.error(function(data) {
@@ -22,19 +54,5 @@ function mainController($scope, $http) {
 		});
 	};
 
-	$scope.chartConfig = {
-		title : 'Productivity',
-		tooltips: true,
-		labels : false,
-/*		mouseover: function() {},
-		mouseout: function() {},
-		click: function() {},*/
-		legend: {
-			display: true,
-		    //could be 'left, right'
-		    position: 'left'
-		}
-	};
 
-	$scope.chartType = 'line';
 }
